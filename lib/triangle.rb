cba3f8f2b366f15168faadf181c122cc01f8ffcb
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-03-23 13:49:14 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,7 @@ class Triangle
   end
   
   def kind
-    valid_triangle
+    # valid_triangle
     if a == b && b == c && c == a
       :equilateral
        elsif a == b || b == c || a == c
@@ -18,12 +18,9 @@ class Triangle
     end
   end
       
-  def validate_triangle
-    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
-    [a, b, c].each { |s| real_triangle << false if s <= 0 }
-    raise TriangleError if real_triangle.include?(false)
+  class TriangleError < StandardError
+    def message 
+      "you must give the get_married method an argument of an instance of the person class!"
+    end
   end
-end
-
-class TriangleError < StandardError
 end
