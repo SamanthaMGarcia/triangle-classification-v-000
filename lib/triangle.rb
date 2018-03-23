88020eb3284488c4f342e79dac5ccf60c2804252
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-03-23 12:34:06 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
-  # write code here
-end
+  attr_reader :a, :b, :c
+  
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+  
+  def kind
+    valid_triangle
+    if a == b && b == c && c == a
+      :equilateral
+       elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+      
+  class TriangleError < StandardError
+    def message 
+      "you must give the get_married method an argument of an instance of the person class!"
+    end
+  end
